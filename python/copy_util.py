eb54b500b5d74555dda38381b4fa00f5e3ff6603
kojiro.tanaka <kojiro.tanaka0406@gmail.com>
2021-04-06 13:33:47 +0900

    modify methods

--- python/copy_util.py
+++ python/copy_util.py
@@ -3,21 +3,26 @@ from pathlib import Path
 import time
 import codecs
 
-from typing import List, Iterable
+from typing import List, Iterable, Callable, Optional
 
 
 class CopytreeIgnore(object):
     """
     Utility for shutil.copytree(), 'ignore' argument.
-    can be used instead of shutil.ignore_patterns().
+    following methods can be used instead of shutil.ignore_patterns().
+      - exclude
+      - include
+      - include_and_exclude
     """
-    log_path: Path = Path().home() / "out.log"
+    DirCall = Callable[[str, List[str]], None]
 
     def __init__(self,
                  in_patterns: Iterable[str] = [],
-                 ex_patterns: Iterable[str] = []):
+                 ex_patterns: Iterable[str] = [],
+                 callback: Optional[DirCall] = None):
         self._set_include_pattern(in_patterns)
         self._set_exclude_pattern(ex_patterns)
+        self._set_callback(callback)
 
     def _set_include_pattern(self, ptn: Iterable[str]) -> None:
         try:
@@ -35,26 +40,19 @@ class CopytreeIgnore(object):
         except TypeError:
             print(ptn, "is not iterable")
 
+    def _set_callback(self, callback: Optional[DirCall]) -> None:
+        self.callback = callback
+
     def exclude(self, directory: str, files: List[str]) -> List[str]:
         """
         callback function for shutil.copytree() 'ignore' argument.
         copytree() only copies that doesn't match glob patterns in
         'ex_patterns'.
         """
-        cwd: Path = Path(directory)
-        # 標準出力
-        print('now copying', str(cwd))
-        # ファイル出力
-        print('now copying', str(cwd), file=codecs.open(
-            str(self.log_path), mode="a"))
-        while True:
-            disk_usage_gb = shutil.disk_usage('C:/').free / 1024 / 1024 / 1024
-            if disk_usage_gb > 20:
-                break
-            print('wait for GDrive Sync...: free is {:>6.2f} GB'.format(disk_usage_gb))
-            # wait for an hour
-            time.sleep(3600)
+        if self.callback:
+            self.callback(directory, files)
 
+        cwd: Path = Path(directory)
         # ignore file/directories list
         ignores: set = set()
         for ptn in self.ex_patterns:
@@ -69,7 +67,7 @@ class CopytreeIgnore(object):
         copytree() only copies that matches glob patterns in 'in_patterns'.
         """
         includes: set = set()
-        cwd = Path(directory)
+        cwd: Path = Path(directory)
         for ptn in self.in_patterns:
             for file_path in cwd.glob(ptn):
                 includes.add(file_path.name)
@@ -84,7 +82,7 @@ class CopytreeIgnore(object):
         """
         excludes: set = set()
         includes: set = set()
-        cwd = Path(directory)
+        cwd: Path = Path(directory)
         if self.in_patterns:
             for ptn in self.in_patterns:
                 for file_path in cwd.glob(ptn):
@@ -105,6 +103,28 @@ class CopytreeIgnore(object):
         pass
 
 
+def wait_for_gdrive_sync(directory: str, files: List[str]) -> None:
+    """
+    it is used for FileSream upload, more than C drive capacity.
+    """
+    # save out.log in home directory
+    log_path: Path = Path().home() / 'out.log'
+    cwd: Path = Path(directory)
+    # print next copy directory
+    print('now copying', str(cwd))
+    # print next copy directory (file output)
+    print('now copying', str(cwd), file=codecs.open(str(log_path), mode="a"))
+    while True:
+        # if C drive(default FileStream cache) capacity is less than 20GB,
+        # wait for google drive sync and delete cache file.
+        disk_usage_gb = shutil.disk_usage('C:/').free / 1024 / 1024 / 1024
+        if disk_usage_gb > 20:
+            break
+        print('wait for GDrive Sync...: free is {:>6.2f} GB'.format(disk_usage_gb))
+        # wait for an hour
+        time.sleep(3600)
+
+
 def main():
     ex_list = [
         '.svn',
@@ -115,7 +135,7 @@ def main():
     srs = Path("C:/Workspace/Scripts/scripts/vba_macro")
     dst = Path("C:/Workspace/Scripts/scripts/test")
     # ------------------------------------------------
-    IgnPtn = CopytreeIgnore(ex_patterns=iter(ex_list))
+    IgnPtn = CopytreeIgnore(ex_patterns=iter(ex_list), callback=wait_for_gdrive_sync)
     shutil.copytree(srs, dst, ignore=IgnPtn.include_and_exclude,
                     dirs_exist_ok=True)
 
